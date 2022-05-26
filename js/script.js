6b4343bbd69d56725f11a4f8b481dd101609f7dd
DevTinel <davidaugustolol36@gmail.com>
2022-05-26 19:57:09 -0300

    js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,45 @@
-const links = document.querySelectorAll(".header-menu a")
+// ativar menu
+const links = document.querySelectorAll(".header-menu a");
 
-
-function ativarLink(link){
-    const url = location.href
-    const href = link.href
-    if (url.includes(href)){
-        link.classList.add("ativo")
-    }
-
+function ativarLink(link) {
+  const url = location.href;
+  const href = link.href;
+  if (url.includes(href)) {
+    link.classList.add("ativo");
+  }
 }
-links.forEach(ativarLink)
+links.forEach(ativarLink);
+
+// ativar items orçamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarParametro(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+parametros.forEach(ativarParametro);
+
+// perguntas frequentes
+
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains('ativa')
+resposta.setAttribute("aria-expanded",ativa)
+  console.log(resposta);
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
